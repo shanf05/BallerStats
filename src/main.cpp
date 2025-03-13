44820c7edfa628b0e9e54c2fa4a3486778c8db25
Severin Hanf <Severin.Hanf@gmail.com>
2025-03-13 01:29:40 +0100

    now really stable build (last faulty), reorganized functions, added .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -5,90 +5,70 @@
 #include <QLabel>
 #include <QScreen>
 #include <stdio.h>
+#include <string>
+#include <vector>
 
-enum opMode {
-    -admin;
-    -user;
-    -debug;
-}
+enum OpMode {
+    admin,
+    user,
+    debug
+};
 
-private class Operator{
+int BuildGui(int argc, char *argv[]);
+OpMode GetOpMode(int argc, char *argv[]);
 
-    opMode m_mode;
-    String m_sName;
-    String m_sPw;
+class Operator{
+    OpMode mode;
+    std::string name;
+    std::string password;
 
-
-
-    Operator* Operator(opMode mode, Strin sName, Strinf sPw) : m_mode(mode), m_sName(sName), m_sPw(sPw){
-        return;
+    public:
+    Operator(OpMode e_mode, std::string s_name, std::string s_password) : mode(e_mode), name(s_name), password(s_password){
+        //iefgi
     }
-}
 
+};
 
+int checkLogin(std::string s_name, std::string s_password){
+    return 0;
+};
 
 int main(int argc, char *argv[]) {
-    opMode m_mode;
 
-    int checkLogin(String sName, String sPw){
+    BuildGui(argc, argv);
 
-        for (int i = 0; i < m_vOperatorList.length(); ; i++)
-        {
-            return (sName = m_sName && nPw = m_sPw);
-            m_vOperatorList
-        }
+    OpMode mode = GetOpMode(argc, argv);
+    std::vector<Operator> OpList;
+    OpList.push_back(Operator(admin, "admin", "1234"));
+}
 
-    }
-
-
+OpMode GetOpMode(int argc, char *argv[]){
+    OpMode mode;
     if(argc > 1){
         for (int i = 0; i < argc; i++){
             std::string arg = argv[i];
 
             if(arg == "-admin"){
-                if(checkLogin("admin", argv[i+1])) m_mode = admin;
+                if(checkLogin("admin", argv[i+1])) mode = admin;
             }
             if(arg == "-user"){
-                if(checkLogin(argv[i+1], argv[i+2])) m_mode = user;
+                if(checkLogin(argv[i+1], argv[i+2])) mode = user;
             }
             if(arg == "-debug"){
-                m_mode = debug;
+                mode = debug;
             }
         }
     }
 
-
-
-
-    std::vector<Operator> m_vOperatorList;
-    m_vOperatorList.add(Operator(admin, "admin", "1234"));
-
-    if(argv[0] == "-admin"){
-        if()
-
-    }
-
-    switch (expression)
-    {
-    case constant expression:
-        /* code */
-        break;
-
-    default:
-        break;
-    }
-
+    return user;
 }
 
+int BuildGui(int argc, char *argv[]){
 
-void build_gui(mode){
     QApplication app(argc, argv);
-
     QWidget window;
     window.setWindowTitle("BallerStats");
-
     window.setFixedSize(800, 600);
-
     QScreen *screen = QGuiApplication::primaryScreen();
     QRect screenGeometry = screen->geometry();
     int screenWidth = screenGeometry.width();
@@ -96,23 +76,15 @@ void build_gui(mode){
     int x = (screenWidth - window.width()) / 2;
     int y = (screenHeight - window.height()) / 2;
     window.move(x, y);
-
-
     QVBoxLayout *layout = new QVBoxLayout;
-
-        QLabel *label = new QLabel("Label");
-        layout->addWidget(label);
-
-        QPushButton *button = new QPushButton("Button");
-        layout->addWidget(button);
-
-        QObject::connect(button, &QPushButton::clicked, [&]() {
-            label->setText("Sie haben den Button geklickt!");
-        });
-
+    QLabel *label = new QLabel("Label");
+    layout->addWidget(label);
+    QPushButton *button = new QPushButton("Button");
+    layout->addWidget(button);
+    QObject::connect(button, &QPushButton::clicked, [&]() {
+        label->setText("Sie haben den Button geklickt!");
+    });
     window.setLayout(layout);
-
-
     window.show();
     return app.exec();
 }
